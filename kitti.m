967cce0d7344acad601df6d3551dee212364751d
zjaxxon <zjaxxon@gmail.com>
2023-10-02 17:37:57 -0400

    param change

--- kitti.m
+++ kitti.m
@@ -2,9 +2,10 @@ clc
 close all
 clear all
 
-M = 45; % dataset max index
+seq = '07';
+M =1090; % dataset max index 7:1090 0:4504 05:2741
 % Construct file names to read the KITTI LiDAR data files jumping 5
-fname = arrayfun(@(idx) sprintf('./kitti/%d.pcd', idx), 0:5:M, 'uniformoutput', false);
+fname = arrayfun(@(idx) sprintf('/media/sdg1/rzh/kitti_lidar/dataset/sequences/%s/pcds/%d.pcd', seq, idx), 0:1:M, 'uniformoutput', false);
 
 fprintf('KITTI LiDAR data loading from specified directory.\n');
 
@@ -15,17 +16,24 @@ df = 1; % downsample degree
 [V, ~] = cellfun(@(V) deal(V.Location(1:df:end,:)'), V, 'uniformoutput', false);
 
 % Initialize parameters for the jrmpc function
-maxNumIter = 100; % Maximum number of iterations
-epsilon = 1e-6;   % Artificial covariance flatten
+maxNumIter = 200; % Maximum number of iterations
+epsilon = 1e-5;   % Artificial covariance flatten
 updatePriors = 0; % Do not update priors during iterations
 
-Ri = eye(3);  % Initial guess for rotation matrix
-ti = zeros(3, 1);  % Initial guess for translation vector
-Xin = V{1}(:,unique(round(linspace(1,size(V{1},2),450))));
-accum_matrix = eye(4);
+R1 = [1.0 0.0 0.0;
+      0.0 -1.0 0.0; 
+      0.0 0.0 -1.0];  % Initial guess for rotation matrix
+R2 = R1;
+t1 = zeros(3, 1);  % Initial guess for translation vector
+t2 = t1;
+Xin = V{2}(:,unique(round(linspace(1,size(V{1},2),450))));
+accum_matrix = [1.0 0.0 0.0 0.0; 
+                0.0 -1.0 0.0 0.0;
+                0.0 0.0 -1.0 0.0;
+                0.0 0.0 0.0 0.1];
 
 % Specify the output file
-outputFilePath = './res/poses07.txt';
+outputFilePath = sprintf('./res/poses%s.txt', seq);
 fid = fopen(outputFilePath, 'w');
 
 fprintf('Registration Start\n');
@@ -35,12 +43,14 @@ for j = 2:numel(V)
     v2 = V{j};
 
     % Call jrmpc on two consecutive frames
-    [R, t, Xout] = jrmpc({v1, v2}, Xin, 'R',{Ri, Ri}, 't', {ti, ti}, 'maxNumIter', maxNumIter, 'epsilon', epsilon, 'updatePriors', updatePriors);
+    [R, t, Xout] = jrmpc({v1, v2}, Xin, 'R',{R1, R2}, 't', {t1, t2}, 'gamma', 0.1, 'maxNumIter', maxNumIter, 'epsilon', epsilon, 'updatePriors', updatePriors);
 
     % Update the guesses
     Xin = Xout;
-    Ri = R{1};
-    ti = t{1};
+    R1 = R{1};
+    R2 = R{2};
+    t1 = t{1};
+    t2 = t{2};
 
     % Solve the transformation matrix
     R1to2 = inv(R{2}) * R{1};
